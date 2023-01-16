386a81095e1fc1a392f30aff73b998f9618c4210
lonzomagill <lonzomagill@gmail.com>
2023-01-16 21:09:12 +0800

    commit hello world example

---
+++ com/jlu/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.jlu;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    private int add (int a, int b) {
+        return a + b;
+    }
+}
