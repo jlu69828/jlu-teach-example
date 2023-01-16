94db6aecde24189b78e0afdd1c435ca7629c265a
lonzomagill <lonzomagill@gmail.com>
2023-01-16 21:07:30 +0800

    commit some teach example

---
+++ com/jlu/TeachExample.java
@@ -0,0 +1,15 @@
+package com.jlu;
+
+/**
+ * add some teach example
+ */
+public class TeachExample {
+
+    private static final String java = "Java";
+
+    public static void main(String[] args) {
+        String love = "I love ";
+        String res = love + java;
+        System.out.println(res);
+    }
+}
